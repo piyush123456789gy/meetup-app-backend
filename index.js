c16d5583a4308070dff4eb5e9b2c54a988eba303
piyush123456789gy <168422795+piyush123456789gy@users.noreply.github.com>
2024-12-03 15:53:42 +0530

    fix: fixing cors

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ initialiseDatabase();
 const cors = require("cors");
 const corsOptions = {
   origin: "http://localhost:5173",
+    methods: "GET,POST,PUT,DELETE", // Allow necessary methods
+
   credentials: true,
   optionSuccessStatus: 200,
 };
