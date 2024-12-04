1474d6db5823538ef4df10aaf41a99458db205ec
piyush.gyl <168422795+piyushgyl01@users.noreply.github.com>
2024-12-04 16:28:27 +0530

    fix: fixing origin link

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ initialiseDatabase();
 
 const cors = require("cors");
 const corsOptions = {
-  origin: "https://frontend-for-meetup-9mq15ntbc-piyush-goyals-projects-72282e7a.vercel.app",
+  origin: "https://frontend-for-meetup-app.vercel.app/",
     methods: "GET,POST,PUT,DELETE", // Allow necessary methods
 
   credentials: true,
